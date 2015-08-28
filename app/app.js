be6d53f5f30d21c9c42e9125ae80053aca7be049
unknown <pljeskavicica@gmail.com>
2015-08-28 11:25:38 +0200

    opet komit

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
  */
 console.log("Hello world!");
 var x={
+	//IGORE NE VALJA TI FUNKCIJA
     add:function(a,b){
         return a+b;
     }
@@ -11,3 +12,5 @@ var x={
 console.log(NOPE);
 
 console.log('Dusanov hello world!');
+
+console.log('novi console.log');
